37008fa2202994340fd435b63c8b9a635a780cc7
Eric Stratton <strattonericj@gmail.com>
2020-10-27 11:09:33 -0700

    add js code to capture loop output in array and print output for user to see
    Co-authored-by: Shannon Grantski <72650879+grantskis@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,15 @@ $(document).ready(function () {
     let countBy = parseInt($("#input2").val());
     console.log(countTo, countBy);
     if (isNaN(countTo) || isNaN(countBy)) {
-      const badAnswer = "Due to current technologies, we can only count by numers :( Please use real numbers.";
-      $(".THIS IS WHERE I WILL PUT THE THING THAT WILL SHOW.... PLACEHOLDER").show();
       $("#bad-answer").show();
     }
     else {
+      $("#bad-answer").hide();
       let output = [];
       for (let number = 0; number <= countTo; number += countBy) {
         output.push(number);
-        console.log(number);
+        $("#output-answer").text(output);
       };
     };
   });
 });
-
-
-// let total = 0;
-// for (let currentNumber = 1; currentNumber <= 5; currentNumber += 1) {
-//   total += currentNumber;
-// }
-// total
